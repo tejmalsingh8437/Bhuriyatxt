12d8a07ced7da18c80e472a2f523c107945bb097
Adity012 <149262704+Adity012@users.noreply.github.com>
2023-12-02 11:31:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "26368249"
 api_hash = "ac2db262d5eb7ce0efc0162404c8d172"
-bot_token = "6970138044:AAFZqpB32j0gFRTqtRtR6mL7fjngHsEDH5Q"
+bot_token = "5834938895:AAFVyBwxJ69EQq2xfncqwRy0hpt4gm67FX4"
