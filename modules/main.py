ec030a27800e1ead85c72f4d805dd47176a93f49
Adity012 <149262704+Adity012@users.noreply.github.com>
2023-12-02 12:10:14 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -162,8 +162,8 @@ async def account_login(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭} {LPRPDV}.mkv\n**Batch »** {raw_text0}\n\n'
-                cc1 = f'** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭} {LPRPDV}.pdf \n**Batch »** {raw_text0}\n\n'
+                cc = f[ 🎬 ]'**Vid_id  »** {str(count).zfill(3)}\n**Title  »** {name1} {res} 🄻🄿🅁🄿🄳🅅.mkv\n**Batch »** {raw_text0}\n**Download by »** {raw_text3}\n\n'
+                cc1 = f[ 📕 ]'**Vid_id  »** {str(count).zfill(3)}\n**Title »** {name1} 🄻🄿🅁🄿🄳🅅.pdf \n**Batch »** {raw_text0}\n**Download by »** {raw_text3}\n\n'
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
