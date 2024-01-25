d9eb4d1cbc68d2ec1592ccf97517428231d1e55a
Sawai12 <109336735+Sawai12@users.noreply.github.com>
2024-01-26 00:05:45 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "27412454"
+itapi_id = "27412454"
 api_hash = "9a657988cb949b70a313e0c84f14207e"
-bot_token = "6919474988:AAFJlqBxUefN7Ss-mHB0_RHJx-5TIpImjTw"
+bot_token = "6808581068:AAGL0eebM8rrCFTDZJd3tXvhiiJqwZ-buvI"
